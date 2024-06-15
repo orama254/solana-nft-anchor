c19e99e1f2ea32f975488b57ea9ce66b9ee37807
orama254 <orama209@gmail.com>
2024-06-15 11:45:04 +0300

    feat: :sparkles: add programID
    Add the generated program id to build and deploy the program

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -13,7 +13,7 @@ use anchor_spl::{
 };
 use mpl_token_metadata::accounts::{ MasterEdition, Metadata as MetadataAccount };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("FRgUY5ZqHRRhViksuHqfobMdKn91oDU7wUopVNSnKaSx");
 #[program]
 pub mod solana_nft_anchor {
     use super::*;
